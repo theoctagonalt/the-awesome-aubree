73070dce3bded58521f6b549b4cd434768b9d066
Max Li <octagonal.tire@gmail.com>
2024-10-25 23:40:55 -0400

    Add boilerplate templates

---
+++ include/globals.h
@@ -0,0 +1,9 @@
+#include "main.h"
+#include "lemlib/api.hpp"
+
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+
+
+#endif
---
+++ include/initialize.h
@@ -0,0 +1,8 @@
+#ifndef INITIALIZE_H_
+#define INITIALIZE_H_
+
+int get_routine();
+void initialize();
+void competition_initialize();
+
+#endif
